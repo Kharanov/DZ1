aac42230ab24d75ba892ca05bad56605d3dd469b
Kharanov <k.haranov@gmail.com>
2022-05-19 19:49:31 +0300

    dz1. reformate

--- src/main/java/DZ/DZ1.java
+++ src/main/java/DZ/DZ1.java
@@ -1,4 +1,5 @@
 package DZ;
+
 public class DZ1 {
     public static void main(String[] args) {
         printThreeWords();
@@ -12,39 +13,39 @@ public class DZ1 {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
-        }
+    }
+
     public static void checkSumSign() {
         int a = 50;
         int b = -70;
         int Sum;
         Sum = a + b;
         if (Sum >= 0) {
-            System.out.println("Summa polozhitelnaya"); }
-        else {
+            System.out.println("Summa polozhitelnaya");
+        } else {
             System.out.println("Summa otricatelnaya");
         }
     }
+
     public static void printColor() {
         int value = 54;
         if (value <= 0) {
             System.out.println("Red");
-        }
-        else if (value > 0 && value <= 100) {
+        } else if (value > 0 && value <= 100) {
             System.out.println("Yellow");
-        }
-        else {
+        } else {
             System.out.println("Green");
         }
     }
+
     public static void compareNumbers() {
         int a = 9;
         int b = 7;
         if (a >= b) {
             System.out.println("a>=b");
-        }
-        else {
+        } else {
             System.out.println("a<b");
         }
 
     }
-    }
+}
